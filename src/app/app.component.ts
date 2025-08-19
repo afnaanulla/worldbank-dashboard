b439af7685869ca3e4bd014fcb202a51a9155ceb
MohammadAfnaan <afnaan31250@gmail.com>
2025-08-19 22:47:29 +0530

    Final Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +10,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'worldbank-dashboard';
+  constructor(private http: HttpClient ) {}
+  ngOnInit() {
+    this.http.get('https://djangoo-1wag.onrender.com/api/auth/csrf/', {
+      withCredentials: true
+    }).subscribe({
+      next: () => console.log('CSRF token fetched Successfully '),
+      error: (err) => console.error('CSRF fetch error', err)
+    });
+  }
 }
