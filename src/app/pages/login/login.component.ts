6370a84b05431574dd6600764824e219f2732c34
MohammadAfnaan <afnaan31250@gmail.com>
2025-08-19 23:45:15 +0530

    ffinal commit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core"
-import { CommonModule } from "@angular/common"
-import { FormsModule } from "@angular/forms"
-import { Router, RouterModule } from "@angular/router"
-import { AuthService } from "../../services/auth.service"
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Router, RouterModule } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   standalone: true,
@@ -11,34 +11,39 @@ import { AuthService } from "../../services/auth.service"
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"],
 })
-export class LoginComponent {
-  username = ""
-  password = ""
-  error = ""
+export class LoginComponent implements OnInit {
+  username = "";
+  password = "";
+  error = "";
+  csrfLoaded = false;
 
-  constructor(
-    private auth: AuthService,
-    private router: Router,
-  ) {
-    this.auth.csrf().subscribe()
+  constructor(private auth: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    // Get CSRF cookie once when component loads
+    this.auth.csrf().subscribe({
+      next: () => {
+        this.csrfLoaded = true;
+        console.log("CSRF cookie set");
+      },
+      error: (err) => {
+        this.error = "Failed to get CSRF token";
+        console.error(err);
+      },
+    });
   }
 
   submit() {
-  // first get CSRF cookie
-  this.auth.csrf().subscribe({
-    next: () => {
-      // then try login
-      this.auth.login({ username: this.username, password: this.password }).subscribe({
-        next: () => this.router.navigate(['/dashboard']),
-        error: (err) => {
-          this.error = err?.error?.detail ?? 'Login failed'
-        }
-      })
-    },
-    error: () => {
-      this.error = 'Failed to get CSRF token'
+    if (!this.csrfLoaded) {
+      this.error = "CSRF token not loaded yet. Please try again.";
+      return;
     }
-  })
-}
 
+    this.auth.login({ username: this.username, password: this.password }).subscribe({
+      next: () => this.router.navigate(["/dashboard"]),
+      error: (err) => {
+        this.error = err?.error?.detail ?? "Login failed";
+      },
+    });
+  }
 }
